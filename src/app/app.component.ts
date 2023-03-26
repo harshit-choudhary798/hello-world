29ede1982c09c00e37a0a78f350aa79904a5c266
Harshit Choudhary <harshit1840077@gmail.com>
2023-03-26 09:41:21 +0530

    app modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
+  post={
+    isActive:true
+  }
+
+
+  working(isActive:boolean){
+    console.log("function called from app component",isActive)
+  }
+
+  tweet={
+    isliked:true,
+    count:0
+  }
+  viewmode:string="map"
+  myVar:string="value1"
+  
+  showMessages=[1,2,3,4,5]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { courseComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
+import { TitlecasePipe } from './titlecase.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikeCountComponent } from './like-count/like-count.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { UdemyCourseComponent } from './udemy-course/udemy-course.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    courseComponent,
+    CourseComponent,
+    SummaryPipe,
+    TitlecasePipe,
+    FavoriteComponent,
+    PanelComponent,
+    LikeCountComponent,
+    ZippyComponent,
+    ContactFormComponent,
+    UdemyCourseComponent,
+    SignUpFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
